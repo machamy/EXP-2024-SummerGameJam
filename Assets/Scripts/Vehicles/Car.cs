ceace8dbf5e29c0db715a4a4c809eb4d124a58cb
fivefar <fivefar@gmail.com>
2024-08-16 23:30:28 +0900

    신호등 프로토타입 구현

--- Assets/Scripts/Vehicles/Car.cs
+++ Assets/Scripts/Vehicles/Car.cs
@@ -5,11 +5,7 @@ using UnityEngine.UI;
 
 public class Car : MonoBehaviour
 {
-    public Color red = Color.red;
-    public Color endColor = Color.green;
-    public float Colorspeed = 1;
-
-    Image imgComp;
+    public SpriteRenderer renderer;
 
     public Vector3 StartPosition;
     public Vector3 EndPosition;
@@ -36,10 +32,21 @@ public class Car : MonoBehaviour
     {
         state = State.stop;
 
+        renderer.color = new Color(1, 1, 1, 1); // 신호등 visible
         // 신호등 띄우고 시간
 
+        yield return new WaitForSeconds(1f); // 대기 시간
 
-        yield return new WaitForSeconds(3f); // 대기 시간
+        renderer.color = Color.red;
+        yield return new WaitForSeconds(1f);
+
+
+        renderer.color = Color.yellow;
+        yield return new WaitForSeconds(1f);
+
+
+        renderer.color = Color.green;
+        yield return new WaitForSeconds(1f);
 
         StartCoroutine(Go());
 
@@ -48,6 +55,9 @@ public class Car : MonoBehaviour
     IEnumerator Go()
     {
         state = State.go;
+
+        renderer.color = new Color(1, 1, 1, 0);
+
         yield return null;
 
     }
@@ -87,12 +97,14 @@ public class Car : MonoBehaviour
     void Awake()
     {
         state = State.start; // 전투 시작알림
-        imgComp = GetComponent<Image>();
     }
 
     // Start is called before the first frame update
     void Start()
     {
+        //renderer = GetComponent<SpriteRenderer>();
+        renderer.color = new Color(1, 1, 1, 0);
+
         collider = GetComponent<Collider2D>();
 
         StartPosition = transform.position;
