8de430cfb11a4526e86b403e35e1f0075dc98c66
machamy <macham2001@gmail.com>
2024-08-16 17:44:08 +0900

    UI매니저 등록 및 점수 갱신 체계

--- Assets/Scripts/VariableSO.cs
+++ Assets/Scripts/VariableSO.cs
@@ -1,3 +1,4 @@
+using DefaultNamespace;
 using UnityEngine;
 using UnityEngine.Events;
 
@@ -6,7 +7,21 @@ namespace DefaultNamespace
     public class VariableSO<T> : ScriptableObject
     {
         private T value;
+
+        public T Value{
+            get => value;
+            set {
+                ValueChangeEvent.Invoke(this.value);
+                this.value = value;
+            }
+        }
+
+        /// <summary>
+        /// 이전 값이 인자로 들어간다.
+        /// </summary>
         public UnityEvent<T> ValueChangeEvent;
+        
+        
     }
 
     public class IntVariableSO : VariableSO<int>
